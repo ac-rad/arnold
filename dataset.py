1f811b80dd9f19ec84fbacf27a45f3f40acc543f
amirhossein-razlighi <arazlighi@gmail.com>
2023-09-12 16:45:52 -0400

    add heightmap functions to dataset.py

--- dataset.py
+++ dataset.py
@@ -6,7 +6,6 @@ import numpy as np
 import torch.nn as nn
 from torch.utils.data import Dataset
 from scipy.spatial.transform import Rotation as R
-from cliport6d.utils.utils import get_fused_heightmap
 from utils.transforms import *
 import clip
 from tqdm import tqdm
@@ -66,6 +65,68 @@ def quaternion_to_discrete_euler(quaternion, resolution):
     disc[disc == int(360 / resolution)] = 0
     return disc
 
+# -----------------------------------------------------------------------------
+# HEIGHTMAP UTILS
+# -----------------------------------------------------------------------------
+
+def get_fused_heightmap(colors, points, bounds, pix_size):
+    """Reconstruct orthographic heightmaps with segmentation masks."""
+    heightmaps, colormaps = [], []
+    for p,i in zip(points, colors):
+        h, c_map = get_heightmap(p, i, bounds, pix_size)
+        heightmaps.append(h)
+        colormaps.append(c_map)
+    colormaps = np.float32(colormaps)
+    heightmaps = np.float32(heightmaps)
+
+    # Fuse maps from different views.
+    valid = np.sum(colormaps, axis=3) > 0
+    repeat = np.sum(valid, axis=0)
+    repeat[repeat == 0] = 1
+    cmap = np.sum(colormaps, axis=0) / repeat[Ellipsis, None]
+    cmap = np.uint8(np.round(cmap))
+    hmap = np.max(heightmaps, axis=0)  # Max to handle occlusions.
+    return cmap, hmap
+
+def get_heightmap(points, colors, bounds, pixel_size):
+    """Get top-down (z-axis) orthographic heightmap image from 3D pointcloud.
+  
+    Args:
+      points: HxWx3 float array of 3D points in world coordinates.
+      colors: HxWx3 uint8 array of values in range 0-255 aligned with points.
+      bounds: 3x2 float array of values (rows: X,Y,Z; columns: min,max) defining
+        region in 3D space to generate heightmap in world coordinates.
+      pixel_size: float defining size of each pixel in meters.
+  
+    Returns:
+      heightmap: HxW float array of height (from lower z-bound) in meters.
+      colormap: HxWx3 uint8 array of backprojected color aligned with heightmap.
+    """
+    width = int(np.round((bounds[0, 1] - bounds[0, 0]) / pixel_size))
+    height = int(np.round((bounds[1, 1] - bounds[1, 0]) / pixel_size))
+    heightmap = np.zeros((height, width), dtype=np.float32)
+    colormap = np.zeros((height, width, colors.shape[-1]), dtype=np.uint8)
+
+    # Filter out 3D points that are outside of the predefined bounds.
+    ix = (points[Ellipsis, 0] >= bounds[0, 0]) & (points[Ellipsis, 0] < bounds[0, 1])
+    iy = (points[Ellipsis, 1] >= bounds[1, 0]) & (points[Ellipsis, 1] < bounds[1, 1])
+    iz = (points[Ellipsis, 2] >= bounds[2, 0]) & (points[Ellipsis, 2] < bounds[2, 1])
+    valid = ix & iy & iz
+    points = points[valid]
+    colors = colors[valid]
+
+    # Sort 3D points by z-value, which works with array assignment to simulate
+    # z-buffering for rendering the heightmap image.
+    iz = np.argsort(points[:, -1])
+    points, colors = points[iz], colors[iz]
+    px = np.int32(np.floor((points[:, 0] - bounds[0, 0]) / pixel_size))
+    py = np.int32(np.floor((points[:, 1] - bounds[1, 0]) / pixel_size))
+    px = np.clip(px, 0, width - 1)
+    py = np.clip(py, 0, height - 1)
+    heightmap[py, px] = points[:, 2] - bounds[2, 0]
+    for c in range(colors.shape[-1]):
+        colormap[py, px, c] = colors[:, c]
+    return heightmap, colormap
 
 def parse_state(filename):
     units = filename.split('-')
